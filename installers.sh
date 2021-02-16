6571a3b21621d06442943d866210b4ee02bf0895
theodson <theodson@gmail.com>
2021-02-16 15:40:14 +1100

    Update pghashlib for macos 11.2.1

--- installers.sh
+++ installers.sh
@@ -3,13 +3,15 @@
 function install_postgres_hashlib() {
     echo 'masOs hashlib installer'
 
-    # prepare of macOs Mojave Oct 2019 - commandline-tools installed 10.15.sdk not 10.14!
-    # some macOs versions may not have the MacOSX10.14.sdk as required by brew's postgresql@9.5
-    # postgresql@9.5 was built against 10.14.sdk as can be seen with `pg_config` - without this link header files are missing.
-    pushd /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/ && \
-    [ ! -e MacOSX10.14.sdk ] && \
-    [ -e MacOSX.sdk ] && \
-    sudo ln -snf MacOSX.sdk MacOSX10.14.sdk
+    # As of macos 11.2.1 BigSur update -
+    # for postgresql-9.5 to build it relies on MacOSX11.0 (possibly make / build overrides available - didnt look into that)
+    # see /usr/local/Cellar/postgresql@9.5/9.5.24/lib/pgxs/src/Makefile.global
+    # But for now, I do have SDK MacOSX11.1.sdk installed but not the required MacOSX11.0.sdk.
+    # So lets assume similiar .h files between the two and create a symlink to satisfy make.
+    [ -e /Library/Developer/CommandLineTools/SDKs/MacOSX11.1.sdk ] && \
+      sudo ln -snf /Library/Developer/CommandLineTools/SDKs/MacOSX11.1.sdk /Library/Developer/CommandLineTools/SDKs/MacOSX11.0.sdk || \
+      echo -e "Need to get MacOSX11.0.sdk or MacOSX11.1.sdk - try \nxcode-select --install"
+
 
     cd /tmp && \
     wget --quiet https://github.com/markokr/pghashlib/archive/master.zip -O pghashlib.zip && \
@@ -23,7 +25,8 @@ function install_postgres_hashlib() {
     rm -f pghashlib.zip
 
     # based on the pghashlib src/test we can check for succesful installation.
-    psql -d postgres -c "create extension hashlib" && \
+    psql -c "drop extension hashlib" || true;
+    psql -c "create extension hashlib" || true;
     [ $(psql -U postgres -t -c"select encode(hash128_string('abcdefg', 'murmur3'), 'hex');" | head -1 | awk '{print $1}') == '069b3c88000000000000000000000000' ] && echo 'pghashlib installed correctly' || 'pghashlib not installed correctly'
 
 }
